69a81378ec00652eba869e49f0e7248cb3a32271
Artur Ptaszek <artur.ptaszek@gmail.com>
2022-04-02 22:29:52 +0200

    feat: Change to plain http client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("http://localhost:4242/health")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
